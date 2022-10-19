d6ff6d408065f9dcf554b3ef229150460295b4b1
zach <zachchou016@gmail.com>
2022-10-19 09:20:10 +0800

    fix web

--- the-stripes-nft-contract/contracts/ThePoorGhostNFT.sol
+++ the-stripes-nft-contract/contracts/ThePoorGhostNFT.sol
@@ -8,7 +8,7 @@ import "contracts/access/Ownable.sol";
 contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
     bool private _blindBoxOpened = false;
-    string private _blindTokenURI = "ipfs://QmexqcLDvoP6HCTtSGumG3yzhZdH8guvV3z3kReCvf2QKn"; // 盲盒uri
+    string private _blindTokenURI = "ipfs://QmR6Kj8XRwmZDPVfFweAErJbbrdU2jKNCkeNksVmFErJ1P"; // 盲盒uri
 
     function _isBlindBoxOpened() internal view returns (bool) {
         return _blindBoxOpened;
@@ -21,16 +21,12 @@ contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
     string public baseExtension = ".json";
     uint256 public cost = 0.02 ether;
     uint256 public maxSupply = 10000;
-    uint256 public maxMintAmount = 20;
     bool public paused = false;
-    mapping(address => bool) public whitelisted;
-    mapping(address => bool) public presaleWallets;
 
     constructor(
         string memory _initBaseURI
     ) ERC721("Poor Ghost", "PG") {
         setBaseURI(_initBaseURI);
-        mint(msg.sender, 20);
     }
 
     // internal
@@ -43,14 +39,11 @@ contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
         uint256 supply = totalSupply();
         require(!paused);
         require(_mintAmount > 0);
-        require(_mintAmount <= maxMintAmount);
+        // require(_mintAmount <= maxMintAmount);
         require(supply + _mintAmount <= maxSupply);
 
         if (msg.sender != owner()) {
-            if (whitelisted[msg.sender] != true) {
-                 //general public
-                require(msg.value >= cost * _mintAmount);
-            }
+            require(msg.value >= cost * _mintAmount);
         }
 
         for (uint256 i = 1; i <= _mintAmount; i++) {
@@ -85,7 +78,7 @@ contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
         string memory currentBaseURI = _baseURI();
         if (_blindBoxOpened) {
             return
-                bytes(baseURI).length > 0
+                bytes(currentBaseURI).length > 0
                     ? string(
                         abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)
                     )
@@ -98,10 +91,6 @@ contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
     //only owner
     function setCost(uint256 _newCost) public onlyOwner {
         cost = _newCost;
-    }
-
-    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-        maxMintAmount = _newmaxMintAmount;
     }
 
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
@@ -117,28 +106,6 @@ contract ThePoorGhostNFT is ERC721Enumerable, Ownable {
 
     function pause(bool _state) public onlyOwner {
         paused = _state;
-    }
-
-    function whitelistUser(address _user) public onlyOwner {
-        whitelisted[_user] = true;
-    }
-
-    function removeWhitelistUser(address _user) public onlyOwner {
-        whitelisted[_user] = false;
-    }
-
-    function addPresaleUser(address _user) public onlyOwner {
-        presaleWallets[_user] = true;
-    }
-
-    function add100PresaleUsers(address[100] memory _users) public onlyOwner {
-        for (uint256 i = 0; i < 2; i++) {
-            presaleWallets[_users[i]] = true;
-        }
-    }
-
-    function removePresaleUser(address _user) public onlyOwner {
-        presaleWallets[_user] = false;
     }
 
     function withdraw() public payable onlyOwner {
