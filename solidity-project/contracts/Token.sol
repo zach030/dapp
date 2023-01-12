d7af3741ab3221da9f6e23e44a73a3b064a48b75
zach <zachchou016@gmail.com>
2023-01-12 08:54:37 +0800

    add safemath and transfer check

--- solidity-project/contracts/Token.sol
+++ solidity-project/contracts/Token.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 import "./CustomLib.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Token {
     constructor(string memory name,string memory symbol,uint128 price) payable {
@@ -30,7 +31,8 @@ contract Token {
     string private _name;
     string private _symbol;
     uint128 private _price;
-    mapping(address => uint) private balanceOf;
+    using SafeMath for uint256;
+    mapping(address => uint) private _balanceOf;
 
     function totalSupply() view public returns(uint256){
         return _totalSupply;
@@ -48,31 +50,39 @@ contract Token {
         return _price;
     }
 
+    function balanceOf(address _account) view public returns(uint){
+        return _balanceOf[_account];
+    }
+
     function transfer(address to, uint256 value) external returns (bool) {
         require(to != address(0), "Token: transfer to the zero address");
-        require(balanceOf[msg.sender] >= value, "insufficient token to transfer");
-        balanceOf[msg.sender] -= value;
-        balanceOf[to] += value;
+        require(_balanceOf[msg.sender] >= value, "insufficient token to transfer");
+
+        _balanceOf[msg.sender].sub(value);
+        _balanceOf[to].add(value);
         emit Transfer(msg.sender, to, value);
         return true;
     }
 
     function mint(address to,uint amount) external returns (bool){
         require(to != address(0), "Token: mint to the zero address");
-        balanceOf[to] += amount;
-        _totalSupply += amount;
+        _balanceOf[to].add(amount);
+        _totalSupply.add(amount);
         emit Mint(to, amount);
         return true;
     }
 
     function sell(uint256 value) external returns(bool){
         require(value > 0 , "Amount to sell must be greater than 0");
-        require(balanceOf[msg.sender] >= value, "insufficient token to sell");
+        require(_balanceOf[msg.sender] >= value, "insufficient token to sell");
         uint256 redeemAmount = value * 600;
         require(address(this).balance > redeemAmount, "insufficient balance to redeem token");
-        _totalSupply -= value;
-        balanceOf[msg.sender] -= value;
-        redeemAmount.customSend(msg.sender);
+        _totalSupply.sub(value);
+        _balanceOf[msg.sender].sub(value);
+        (bool success) = redeemAmount.customSend(msg.sender);
+        if (!success){
+            revert();
+        }
         emit Sell(msg.sender, value);
         return true;
     }
